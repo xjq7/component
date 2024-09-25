1d5064152547c8cd5df4a419f3501ba69fc505b2
xjq <lebnizjoan@gmail.com>
2024-09-25 17:54:01 +0800

    init

---
+++ src/Toast/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export interface ToastProps {}
+
+export default function Toast(props: ToastProps) {
+  return <div>222</div>;
+}
---
+++ src/main.tsx
@@ -0,0 +1,5 @@
+import Toast, { ToastProps } from './Toast';
+
+export { ToastProps };
+
+export { Toast };
